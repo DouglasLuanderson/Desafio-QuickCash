e150498a07fb2ad40d14bfa00579ba7e80ae146b
Douglasyrr18 <douglas.informatica.rj@gmail.com>
2019-06-03 02:15:16 -0300

    ajustes em fonts e slide concluido

--- main.js
+++ main.js
@@ -1,3 +1,89 @@
+// slide history
+
+$(document).ready(function ($) {
+
+    $('#checkbox').change(function(){
+      setInterval(function () {
+          moveRight();
+      }, 3000);
+    });
+    
+    var slideCount = $('#slide .slide-block .slide-block-image').length;
+    var slideWidth = $('#slide .slide-block .slide-block-image');
+    var slideHeight = $('#slide .slide-block .slide-block-image');
+    var sliderUlWidth = slideCount * slideWidth;
+    
+    $('#slide').css({ width: slideWidth, height: slideHeight });
+    
+    $('#slide .slide-block').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+    
+    $('#slide .slide-block .slide-block-image:last-child').prependTo('#slide .slide-block');
+
+    function moveLeft() {
+        $('#slide .slide-block').animate({
+            left: + slideWidth
+        }, 200, function () {
+            $('#slide .slide-block img:last-child').prependTo('#slide .slide-block');
+            $('#slide .slide-block').css('left', '');
+        });
+    };
+
+    function moveRight() {
+        $('#slide .slide-block').animate({
+            left: - slideWidth
+        }, 200, function () {
+            $('#slide .slide-block .slide-block-image:first-child').appendTo('#slide .slide-block');
+            $('#slide .slide-block').css('left', '');
+        });
+    };
+
+    $('.slide-block-arrowRight').click(function () {
+        moveLeft();
+    });
+
+    $('.slide-block-arrowLeft').click(function () {
+        moveRight();
+    });
+
+});   
+
+// menu mobile
+
+$(function(){
+	
+	$('.navbarMobile-text').click(function(){
+		$('.navbarMobile-menu').css('display','flex');
+	});
+	$('.botaoSair').click(function(){
+		$('.navbarMobile-menu').css('display','none');
+	});
+	$('.contatos').click(function(){
+		$('.navbarMobile-menu').css('display','none');
+    });
+});
+
+// button header
+
+$(function(){      
+    $('.close').click(function(){
+        $('form').css('display','none');
+    });
+    $('.banner-contact').click(function(){
+        $('form').css('display','block');
+    });
+    $('.close').click(function(){
+        $('form').css('display','none');
+    });
+    $('.close').clickout(function(){
+        $('.banner-contact').css('display','flex');
+    });
+    $('.banner-contact').click(function(){
+        $('.banner-contact').css('display','none');
+    }); 
+})
+
+// convert
+
 $( document ).ready(function() {
     console.log( "ready!" );
 
@@ -24,18 +110,6 @@ $( document ).ready(function() {
 
 });
 
-$(function(){
-	
-	$('.navbarMobile-text').click(function(){
-		$('.navbarMobile-menu').css('display','flex');
-	});
-	$('.botaoSair').click(function(){
-		$('.navbarMobile-menu').css('display','none');
-	});
-	$('.contatos').click(function(){
-		$('.navbarMobile-menu').css('display','none');
-	});
-});
 
 
 
